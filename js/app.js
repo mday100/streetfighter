53dfe3e5abd36efac728cb6a8361d88d185d8ae2
Matt Day <mday100@live.com>
2014-09-21 11:56:05 -0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+	intro();
+	$('.ryu').mouseenter(function() {
+		$('.ryu-still').hide();
+		$('.ryu-ready').show();
+	})
+	.mouseleave(function() {
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
+	})
+	.mousedown(function() {
+		playHadouken();
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').finish().show()
+		.animate(
+			{ 'left': '300px'}, 
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left', '-212px');
+			});
+	})
+	.mouseup(function() {
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
+	});
+
+	$(document).keydown(function(event) {
+		if (event.which == 88) {
+			$('.ryu-still').hide();
+			$('.ryu-ready').hide();
+			$('.ryu-throwing').hide();
+			$('.ryu-cool').show();
+		}
+	})
+	.keyup(function(event) {
+		if (event.which == 88) {
+			$('.ryu-cool').hide();
+			$('.ryu-still').show();
+		}
+	});
+})
+
+function playHadouken() {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
+
+function intro() {
+	$('#theme-song')[0].volume = 0.3;
+  	$('#theme-song')[0].play();
+  	$('.sf-logo').fadeIn(3500, function() {
+    	$(this).fadeOut(1000, function() {
+      		$('.how-to').fadeIn(1500, function() {
+            });
+         })
+    })
+}
